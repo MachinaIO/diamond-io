49a721c849f065457dce9d277f148fedef4bc48f
SoraSuegami <suegamisora@gmail.com>
2025-03-05 16:10:54 +0900

    Remove unused files under bgg

--- src/bgg/mod.rs
+++ src/bgg/mod.rs
@@ -1,12 +1,6 @@
-// pub mod ciphertext;
-// pub mod circuit;
-// pub mod operations;
-// pub mod parameters;
-pub mod eval;
-pub mod sampler;
-
 pub mod encoding;
 pub mod public_key;
+pub mod sampler;
 
 pub use encoding::BggEncoding;
 pub use public_key::BggPublicKey;
