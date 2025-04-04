5131b992dd72326415fe24c5c767d30dcdf17ee4
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2025-04-04 15:30:56 +0200

    nit: rename variables

--- src/bgg/circuit/mod.rs
+++ src/bgg/circuit/mod.rs
@@ -427,7 +427,6 @@ mod tests {
         // Define a specific bit pattern
         // This will create a polynomial with coefficients:
         // [1, 0, 1, 1]
-        // (where 1 is at positions 0, 2, 3, and 4)
         let bits = vec![true, false, true, true];
         let bit_poly_gate = circuit.const_bit_poly(&bits);
         circuit.output(vec![bit_poly_gate]);
@@ -439,7 +438,7 @@ mod tests {
         // Verify the result
         assert_eq!(result.len(), 1);
 
-        // Check that the coefficients match the bit pattern
+        // Check that the first four coefficients match the bit pattern
         let coeffs = result[0].coeffs();
         for (i, bit) in bits.iter().enumerate() {
             if *bit {
@@ -725,55 +724,54 @@ mod tests {
         let mut circuit = PolyCircuit::new();
         let uniform_sampler = DCRTPolyUniformSampler::new();
 
-        // encrypt a polynomial m using a RLWE secret key encryption
-        // c0 = a*s + e + m (where m is the plaintext polynomial)
-        // c1 = -a
-        let m = create_bit_random_poly(&params);
-        let s = uniform_sampler.sample_poly(&params, &DistType::BitDist);
+        // encrypt a polynomial k using a RLWE secret key encryption
+        // b = a*t + e + k
+        let k = create_bit_random_poly(&params);
+        let t = uniform_sampler.sample_poly(&params, &DistType::BitDist);
         let e = uniform_sampler.sample_poly(&params, &DistType::GaussDist { sigma: 0.0 });
         let a = uniform_sampler.sample_poly(&params, &DistType::FinRingDist);
-        let c0 = -a.clone();
-        let c1 = a * s.clone() + e + m.clone();
+        let b = a.clone() * t.clone() + e + k.clone();
 
-        // k is a polynomial from bit distribution
-        let k = uniform_sampler.sample_poly(&params, &DistType::BitDist);
+        // x is a polynomial from bit distribution
+        let x = uniform_sampler.sample_poly(&params, &DistType::BitDist);
 
-        let c0_bits = c0.decompose(&params);
-        let c1_bits = c1.decompose(&params);
+        let a_bits = a.decompose(&params);
+        let b_bits = b.decompose(&params);
 
-        let inputs = circuit.input(c0_bits.len() + c1_bits.len() + 1);
+        // inputs are a_bits, b_bits, x
+        let inputs = circuit.input(a_bits.len() + b_bits.len() + 1);
         assert_eq!(inputs.len(), params.modulus_bits() * 2 + 1);
 
-        // Input: c0_bits[0], ..., c0_bits[modulus_bits - 1], c1_bits[0], ..., c1_bits[modulus_bits
-        // - 1], k Output: c0_bits[0] * k, ..., c0_bits[modulus_bits - 1] * k, c1_bits[0] *
-        // k, ..., c1_bits[modulus_bits - 1] * k
-        let k_id = inputs[inputs.len() - 1];
+        // Input: a_bits[0], ..., a_bits[modulus_bits - 1], b_bits[0], ..., b_bits[modulus_bits -
+        // 1], x Output: a_bits[0] * x, ..., a_bits[modulus_bits - 1] * x, b_bits[0] * x,
+        // ..., b_bits[modulus_bits - 1] * x
+        let x_id = inputs[inputs.len() - 1];
         let output_ids = inputs
             .iter()
             .take(inputs.len() - 1)
-            .map(|&input_id| circuit.mul_gate(input_id, k_id))
+            .map(|&input_id| circuit.mul_gate(input_id, x_id))
             .collect();
 
         circuit.output(output_ids);
 
-        // concatenate decomposed_c0 and decomposed_c1 and k
-        let input = [c0_bits, c1_bits, vec![k.clone()]].concat();
+        // concatenate a_bits and b_bits and x
+        let input = [a_bits, b_bits, vec![x.clone()]].concat();
         let result = circuit.eval(&params, &DCRTPoly::const_one(&params), &input);
 
         assert_eq!(result.len(), params.modulus_bits() * 2);
 
-        let c0_bits_eval = result[..params.modulus_bits()].to_vec();
-        let c1_bits_eval = result[params.modulus_bits()..].to_vec();
+        let a_bits_eval = result[..params.modulus_bits()].to_vec();
+        let b_bits_eval = result[params.modulus_bits()..].to_vec();
 
-        let c0_eval = DCRTPoly::from_decomposed(&params, &c0_bits_eval);
-        let c1_eval = DCRTPoly::from_decomposed(&params, &c1_bits_eval);
+        let a_eval = DCRTPoly::from_decomposed(&params, &a_bits_eval);
+        let b_eval = DCRTPoly::from_decomposed(&params, &b_bits_eval);
 
-        assert_eq!(c0_eval, &c0 * &k);
-        assert_eq!(c1_eval, &c1 * &k);
+        assert_eq!(a_eval, &a * &x);
+        assert_eq!(b_eval, &b * &x);
 
         // decrypt the result
-        let plaintext = c1_eval + c0_eval * s;
-        assert_eq!(plaintext, m * k);
+        let plaintext = b_eval - (a_eval * t);
+        assert_eq!(plaintext, k * x);
     }
 
     #[test]
