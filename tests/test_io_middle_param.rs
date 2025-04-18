82c087d6bde065a26e79825c4ea52b0d101a50fe
SoraSuegami <suegamisora@gmail.com>
2025-04-18 13:36:55 +0900

    Remove warnings in tests

--- tests/test_io_middle_param.rs
+++ tests/test_io_middle_param.rs
@@ -1,25 +1,5 @@
 #[cfg(test)]
 mod test {
-    use diamond_io::{
-        bgg::circuit::PolyCircuit,
-        io::{obf::obfuscate, params::ObfuscationParams},
-        poly::{
-            dcrt::{
-                DCRTPoly, DCRTPolyHashSampler, DCRTPolyMatrix, DCRTPolyParams,
-                DCRTPolyTrapdoorSampler, DCRTPolyUniformSampler, FinRingElem,
-            },
-            sampler::DistType,
-            Poly, PolyElem, PolyParams,
-        },
-        utils::init_tracing,
-    };
-    use keccak_asm::Keccak256;
-    use num_bigint::BigUint;
-    use num_traits::Num;
-    use rand::Rng;
-    use std::sync::Arc;
-    use tracing::info;
-
     use diamond_io::test_utils::test_io_common;
 
     #[tokio::test]
