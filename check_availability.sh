5773c90729855493a3cea495ca63f81c2c84a9ce
Nick Tampakas <nick@gmx.co.uk>
2025-05-08 20:15:04 +0300

    Fix functionality

--- check_availability.sh
+++ check_availability.sh
@@ -14,9 +14,31 @@ for SUBNET_ID in $SUBNET_IDS; do
   AZ=$(aws ec2 describe-subnets --subnet-ids "$SUBNET_ID" --region "$REGION" --query "Subnets[0].AvailabilityZone" --output text 2>/dev/null)
   [ -z "$AZ" ] && continue
 
-  IS_AVAILABLE=$(aws ec2 describe-instance-type-offerings --region "$REGION" --location-type availability-zone --filters Name=location,Values="$AZ" Name=instance-type,Values="$INSTANCE_TYPE" --query "InstanceTypeOfferings[].InstanceType" --output text 2>/dev/null)
-  [ -n "$IS_AVAILABLE" ] && { SELECTED_SUBNET="$SUBNET_ID"; break; }
+  RESULT=$(aws ec2 run-instances \
+    --region "$REGION" \
+    --instance-type "$INSTANCE_TYPE" \
+    --image-id "$IMAGE_ID" \
+    --subnet-id "$SUBNET_ID" \
+    --security-group-ids $SG \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=ProjectName,Value=machina-io}]" "ResourceType=volume,Tags=[{Key=ProjectName,Value=machina-io}]" \
+    --count 1 \
+    --dry-run \
+    --query "Instances[0].InstanceId" \
+    --output text 2>&1 || echo "ERROR: $?")
+
+  if [[ "$RESULT" == *"Request would have succeeded, but DryRun flag is set"* ]]; then
+    SELECTED_SUBNET="$SUBNET_ID"
+    SELECTED_AZ="$AZ"
+    break
+  elif [[ "$RESULT" == *"InsufficientInstanceCapacity"* ]]; then
+    echo "Insufficient capacity in $AZ"
+  elif [[ "$RESULT" == *"InvalidParameterCombination"* ]]; then
+    echo "Failed to check capacity in $AZ (Invalid parameter combination: $RESULT)"
+  else
+    echo "Failed to check capacity in $AZ (Error: $RESULT)"
+  fi
 done
+
 
 if [ -n "$SELECTED_SUBNET" ]; then
   echo "$SELECTED_SUBNET"
