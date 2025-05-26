25a0ff7903970f3ed9a1c0480367a734fb6b7549
πa <76558220+rkdud007@users.noreply.github.com>
2025-05-26 15:27:31 +0900

    rm non used utils

--- src/utils.rs
+++ src/utils.rs
@@ -5,11 +5,8 @@ use std::{thread, time};
 use crate::poly::{
     dcrt::{DCRTPoly, DCRTPolyParams, DCRTPolyUniformSampler},
     sampler::{DistType, PolyUniformSampler},
-    Poly,
 };
 use memory_stats::memory_stats;
-use num_bigint::BigUint;
-use num_traits::{One, Zero};
 use rayon::prelude::*;
 use std::time::{Duration, Instant};
 #[cfg(feature = "cpu")]
@@ -18,50 +15,6 @@ use sysinfo::{CpuRefreshKind, RefreshKind, System};
 use tempfile::env::temp_dir;
 use tracing::{debug, info};
 use walkdir::WalkDir;
-
-pub fn ceil_log2(q: &BigUint) -> usize {
-    assert!(!q.is_zero(), "log2 is undefined for zero");
-
-    let bits = q.bits() as usize;
-    if q & (q - BigUint::one()) == BigUint::zero() {
-        bits - 1
-    } else {
-        bits
-    }
-}
-
-/// Print a ring element
-pub fn print_ring_element(label: &str, ring_el: &[u64]) {
-    print!("{} [", label);
-    for (k, &val) in ring_el.iter().enumerate() {
-        if k > 0 {
-            print!(", ");
-        }
-        print!("{}", val);
-    }
-    println!("]");
-}
-
-/// Print a matrix of ring elements
-pub fn print_matrix_ring(label: &str, matrix: &[Vec<Vec<u64>>]) {
-    println!("\n{}", label,);
-
-    for (i, row) in matrix.iter().enumerate() {
-        for (j, col) in row.iter().enumerate() {
-            print!("r{}c{}: ", i, j);
-            print_ring_element("", col);
-        }
-    }
-}
-
-/// Print a vector of ring elements
-pub fn print_vector_ring(label: &str, vec: &[Vec<u64>]) {
-    println!("\n{}", label);
-    for (i, inner_vec) in vec.iter().enumerate() {
-        print!("{}[{}]: ", label, i);
-        print_ring_element("", inner_vec);
-    }
-}
 
 // Helper function to create a random polynomial using UniformSampler
 pub fn create_random_poly(params: &DCRTPolyParams) -> DCRTPoly {
@@ -72,15 +25,6 @@ pub fn create_random_poly(params: &DCRTPolyParams) -> DCRTPoly {
 pub fn create_bit_random_poly(params: &DCRTPolyParams) -> DCRTPoly {
     let sampler = DCRTPolyUniformSampler::new();
     sampler.sample_poly(params, &DistType::BitDist)
-}
-
-// Helper function to create a bit polynomial (0 or 1)
-pub fn create_bit_poly(params: &DCRTPolyParams, bit: bool) -> DCRTPoly {
-    if bit {
-        DCRTPoly::const_one(params)
-    } else {
-        DCRTPoly::const_zero(params)
-    }
 }
 
 pub fn log_mem<T: Into<String>>(tag: T) {
