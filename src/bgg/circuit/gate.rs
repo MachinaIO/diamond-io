7e349fbbfe1c5a5045088c34aa751c65279b0da4
πa <76558220+rkdud007@users.noreply.github.com>
2025-07-09 09:13:20 +0900

    fix

--- src/bgg/circuit/gate.rs
+++ src/bgg/circuit/gate.rs
@@ -31,6 +31,5 @@ impl PolyGateType {
             PolyGateType::Add | PolyGateType::Sub | PolyGateType::Mul => 2,
             PolyGateType::Call { num_input, .. } => *num_input,
         }
-        lookup_id
     }
 }
